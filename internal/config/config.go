745ebbc7d7b26d0f823d82bbf8bf04cf3e8aa6db
adoboscan21 <adoboscan21@gmail.com>
2026-03-11 17:57:24 -0400

    LUNADB name put in some files.

--- internal/config/config.go
+++ internal/config/config.go
@@ -59,79 +59,79 @@ func LoadConfig() Config {
 
 // applyEnvConfig overrides config values from environment variables.
 func applyEnvConfig(cfg *Config) {
-	if portEnv := os.Getenv("MEMORYTOOLS_PORT"); portEnv != "" {
+	if portEnv := os.Getenv("LUNADB_PORT"); portEnv != "" {
 		cfg.Port = portEnv
 		slog.Info("Overriding Port from environment", "value", portEnv)
 	}
 
-	if numShardsEnv := os.Getenv("MEMORYTOOLS_NUM_SHARDS"); numShardsEnv != "" {
+	if numShardsEnv := os.Getenv("LUNADB_NUM_SHARDS"); numShardsEnv != "" {
 		if i, err := strconv.Atoi(numShardsEnv); err == nil && i > 0 {
 			cfg.NumShards = i
 			slog.Info("Overriding NumShards from environment", "value", i)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_NUM_SHARDS env var, using default", "value", numShardsEnv)
+			slog.Warn("Invalid LUNADB_NUM_SHARDS env var, using default", "value", numShardsEnv)
 		}
 	}
 
-	if coldMonthsEnv := os.Getenv("MEMORYTOOLS_COLD_STORAGE_MONTHS"); coldMonthsEnv != "" {
+	if coldMonthsEnv := os.Getenv("LUNADB_COLD_STORAGE_MONTHS"); coldMonthsEnv != "" {
 		if i, err := strconv.Atoi(coldMonthsEnv); err == nil && i >= 0 {
 			cfg.ColdStorageMonths = i
 			slog.Info("Overriding ColdStorageMonths from environment", "value", i)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_COLD_STORAGE_MONTHS env var, using default", "value", coldMonthsEnv)
+			slog.Warn("Invalid LUNADB_COLD_STORAGE_MONTHS env var, using default", "value", coldMonthsEnv)
 		}
 	}
 
-	if enableSnapshotsEnv := os.Getenv("MEMORYTOOLS_ENABLE_SNAPSHOTS"); enableSnapshotsEnv != "" {
+	if enableSnapshotsEnv := os.Getenv("LUNADB_ENABLE_SNAPSHOTS"); enableSnapshotsEnv != "" {
 		if b, err := strconv.ParseBool(enableSnapshotsEnv); err == nil {
 			cfg.EnableSnapshots = b
 			slog.Info("Overriding EnableSnapshots from environment", "value", b)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_ENABLE_SNAPSHOTS env var, using default", "value", enableSnapshotsEnv)
+			slog.Warn("Invalid LUNADB_ENABLE_SNAPSHOTS env var, using default", "value", enableSnapshotsEnv)
 		}
 	}
 
-	if rootPassEnv := os.Getenv("MEMORYTOOLS_ROOT_PASSWORD"); rootPassEnv != "" {
+	if rootPassEnv := os.Getenv("LUNADB_ROOT_PASSWORD"); rootPassEnv != "" {
 		cfg.DefaultRootPassword = rootPassEnv
 	}
 
-	if adminPassEnv := os.Getenv("MEMORYTOOLS_ADMIN_PASSWORD"); adminPassEnv != "" {
+	if adminPassEnv := os.Getenv("LUNADB_ADMIN_PASSWORD"); adminPassEnv != "" {
 		cfg.DefaultAdminPassword = adminPassEnv
 	}
 
-	if hotHoursEnv := os.Getenv("MEMORYTOOLS_HOT_STORAGE_CLEAN_HOURS"); hotHoursEnv != "" {
+	if hotHoursEnv := os.Getenv("LUNADB_HOT_STORAGE_CLEAN_HOURS"); hotHoursEnv != "" {
 		if i, err := strconv.Atoi(hotHoursEnv); err == nil && i >= 0 {
 			cfg.HotStorageCleanHours = i
 			slog.Info("Overriding HotStorageCleanHours from environment", "value", i)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_HOT_STORAGE_CLEAN_HOURS env var, using default", "value", hotHoursEnv)
+			slog.Warn("Invalid LUNADB_HOT_STORAGE_CLEAN_HOURS env var, using default", "value", hotHoursEnv)
 		}
 	}
 
-	if workerPoolEnv := os.Getenv("MEMORYTOOLS_WORKER_POOL_SIZE"); workerPoolEnv != "" {
+	if workerPoolEnv := os.Getenv("LUNADB_WORKER_POOL_SIZE"); workerPoolEnv != "" {
 		if i, err := strconv.Atoi(workerPoolEnv); err == nil && i > 0 {
 			cfg.WorkerPoolSize = i
 			slog.Info("Overriding WorkerPoolSize from environment", "value", i)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_WORKER_POOL_SIZE env var, using default", "value", workerPoolEnv)
+			slog.Warn("Invalid LUNADB_WORKER_POOL_SIZE env var, using default", "value", workerPoolEnv)
 		}
 	}
 
-	if enableWalEnv := os.Getenv("MEMORYTOOLS_ENABLE_WAL"); enableWalEnv != "" {
+	if enableWalEnv := os.Getenv("LUNADB_ENABLE_WAL"); enableWalEnv != "" {
 		if b, err := strconv.ParseBool(enableWalEnv); err == nil {
 			cfg.EnableWal = b
 			slog.Info("Overriding EnableWal from environment", "value", b)
 		} else {
-			slog.Warn("Invalid MEMORYTOOLS_ENABLE_WAL env var, using default", "value", enableWalEnv)
+			slog.Warn("Invalid LUNADB_ENABLE_WAL env var, using default", "value", enableWalEnv)
 		}
 	}
 
-	overrideDuration("MEMORYTOOLS_SHUTDOWN_TIMEOUT", &cfg.ShutdownTimeout)
-	overrideDuration("MEMORYTOOLS_SNAPSHOT_INTERVAL", &cfg.SnapshotInterval)
-	overrideDuration("MEMORYTOOLS_TTL_CLEAN_INTERVAL", &cfg.TtlCleanInterval)
-	overrideDuration("MEMORYTOOLS_BACKUP_INTERVAL", &cfg.BackupInterval)
-	overrideDuration("MEMORYTOOLS_BACKUP_RETENTION", &cfg.BackupRetention)
-	overrideDuration("MEMORYTOOLS_WAL_FSYNC_INTERVAL", &cfg.WalFsyncInterval)
+	overrideDuration("LUNADB_SHUTDOWN_TIMEOUT", &cfg.ShutdownTimeout)
+	overrideDuration("LUNADB_SNAPSHOT_INTERVAL", &cfg.SnapshotInterval)
+	overrideDuration("LUNADB_TTL_CLEAN_INTERVAL", &cfg.TtlCleanInterval)
+	overrideDuration("LUNADB_BACKUP_INTERVAL", &cfg.BackupInterval)
+	overrideDuration("LUNADB_BACKUP_RETENTION", &cfg.BackupRetention)
+	overrideDuration("LUNADB_WAL_FSYNC_INTERVAL", &cfg.WalFsyncInterval)
 }
 
 func overrideDuration(envKey string, target *time.Duration) {
